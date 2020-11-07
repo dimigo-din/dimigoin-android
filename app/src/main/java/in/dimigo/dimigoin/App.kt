51e6f73edbaa63c0688c260f4f4a2beb631ef471
SeungHyunMin <contact@seunghyun.in>
2020-11-07 14:35:17 +0900

    chore: configure koin

---
+++ app/src/main/java/in/dimigo/dimigoin/App.kt
@@ -0,0 +1,19 @@
+package `in`.dimigo.dimigoin
+
+import `in`.dimigo.dimigoin.di.appModule
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger()
+            androidContext(this@App)
+            modules(appModule)
+        }
+    }
+}
---
+++ app/src/main/java/in/dimigo/dimigoin/di/AppModule.kt
@@ -0,0 +1,6 @@
+package `in`.dimigo.dimigoin.di
+
+import org.koin.dsl.module
+
+val appModule = module {
+}
