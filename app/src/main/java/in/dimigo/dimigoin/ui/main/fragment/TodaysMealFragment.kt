76957371fa2261ee95b967b1b7309cb440beeae9
wkdwhddn06 <wkdwhddn06@naver.com>
2020-11-07 21:02:55 +0900

    feat: apply databinding on today meal fragment

--- app/src/main/java/in/dimigo/dimigoin/ui/main/fragment/TodaysMealFragment.kt
+++ app/src/main/java/in/dimigo/dimigoin/ui/main/fragment/TodaysMealFragment.kt
@@ -1,5 +1,19 @@
 package `in`.dimigo.dimigoin.ui.main.fragment
 
+import `in`.dimigo.dimigoin.databinding.FragmentSchoolMealBinding
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class TodaysMealFragment : Fragment()
+class TodaysMealFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentSchoolMealBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
