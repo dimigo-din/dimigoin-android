b440b0378d7b9e4dda9edb31ec4d916eacc4af9f
SeungHyunMin <contact@seunghyun.in>
2020-11-07 20:59:48 +0900

    feat: auto login

--- app/src/main/java/in/dimigo/dimigoin/di/AppModule.kt
+++ app/src/main/java/in/dimigo/dimigoin/di/AppModule.kt
@@ -3,6 +3,10 @@ package `in`.dimigo.dimigoin.di
 import `in`.dimigo.dimigoin.data.usecase.AuthUseCase
 import `in`.dimigo.dimigoin.data.usecase.AuthUseCaseImpl
 import `in`.dimigo.dimigoin.ui.login.LoginViewModel
+import android.content.Context
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
+import org.koin.android.ext.koin.androidContext
 import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
@@ -10,4 +14,19 @@ val appModule = module {
     factory<AuthUseCase> { AuthUseCaseImpl(get()) }
 
     viewModel { LoginViewModel(get()) }
+
+    single { createEncryptedSharedPreferences(androidContext()) }
+}
+
+private fun createEncryptedSharedPreferences(context: Context) {
+    val masterKey = MasterKey.Builder(context, MasterKey.DEFAULT_MASTER_KEY_ALIAS)
+        .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+        .build()
+    EncryptedSharedPreferences.create(
+        context,
+        "app_preferences",
+        masterKey,
+        EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+        EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+    )
 }
--- app/src/main/java/in/dimigo/dimigoin/ui/login/LoginActivity.kt
+++ app/src/main/java/in/dimigo/dimigoin/ui/login/LoginActivity.kt
@@ -4,18 +4,25 @@ import `in`.dimigo.dimigoin.R
 import `in`.dimigo.dimigoin.data.model.AuthModel
 import `in`.dimigo.dimigoin.databinding.ActivityLoginBinding
 import `in`.dimigo.dimigoin.ui.util.observeEvent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.edit
 import androidx.databinding.DataBindingUtil
+import org.koin.android.ext.android.inject
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class LoginActivity : AppCompatActivity() {
     private val viewModel: LoginViewModel by viewModel()
+    private val sharedPreferences: SharedPreferences by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_App)
         super.onCreate(savedInstanceState)
+
+        if (tryAutoLogin()) loginFinished()
+
         DataBindingUtil.setContentView<ActivityLoginBinding>(this, R.layout.activity_login).apply {
             lifecycleOwner = this@LoginActivity
             vm = viewModel
@@ -26,9 +33,23 @@ class LoginActivity : AppCompatActivity() {
         })
     }
 
+    private fun loginFinished() {
+        // TODO : start activity
+        finish()
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+    }
+
+    private fun tryAutoLogin(): Boolean {
+        return sharedPreferences.getString(KEY_TOKEN, null) != null
+    }
+
     private fun handleEvent(event: Event) = when (event) {
         is Event.LoginSuccess -> {
-            // TODO : Auto Login, Start Activity
+            sharedPreferences.edit {
+                putString(KEY_TOKEN, event.authModel.token)
+                putString(KEY_REFRESH_TOKEN, event.authModel.refreshToken)
+            }
+            loginFinished()
         }
         is Event.LoginFail -> {
             Toast.makeText(this, R.string.login_failed, Toast.LENGTH_LONG).show()
@@ -38,5 +59,10 @@ class LoginActivity : AppCompatActivity() {
     sealed class Event {
         data class LoginSuccess(val authModel: AuthModel) : Event()
         object LoginFail : Event()
+    }
+
+    companion object {
+        private const val KEY_TOKEN = "id"
+        private const val KEY_REFRESH_TOKEN = "password"
     }
 }
