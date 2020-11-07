be38375c178f7001ff37a09c0cb02ee548302765
SeungHyunMin <contact@seunghyun.in>
2020-11-07 16:47:17 +0900

    feat: add EventWrapper and EventObserver

---
+++ app/src/main/java/in/dimigo/dimigoin/ui/util/EventObserver.kt
@@ -0,0 +1,12 @@
+package `in`.dimigo.dimigoin.ui.util
+
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+
+fun <T> LiveData<EventWrapper<T>>.observeEvent(lifecycleOwner: LifecycleOwner, onChanged: (T) -> Unit) {
+    observe(lifecycleOwner, { eventWrapper ->
+        eventWrapper.getContentIfNotHandled()?.let { value ->
+            onChanged(value)
+        }
+    })
+}
