7ed01d52d61280d6d58e9a73ff1d6780cf162002
wkdwhddn06 <wkdwhddn06@naver.com>
2020-11-08 14:53:53 +0900

    fix: fixed dimibob api issue - #4

--- app/src/main/java/in/dimigo/dimigoin/data/usecase/meal/MealUseCaseImpl.kt
+++ app/src/main/java/in/dimigo/dimigoin/data/usecase/meal/MealUseCaseImpl.kt
@@ -3,9 +3,12 @@ package `in`.dimigo.dimigoin.data.usecase.meal
 import `in`.dimigo.dimigoin.data.api.DimigoinApi
 import `in`.dimigo.dimigoin.data.model.MealModel
 import retrofit2.await
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MealUseCaseImpl(private val api: DimigoinApi) : MealUseCase {
-    override suspend fun getTodaysMeal(): MealModel {
-        return api.getTodaysMeal().await()
+    override suspend fun getMeal(date: Date): MealModel {
+        val formattedDate = SimpleDateFormat("yyyyMMdd", Locale.getDefault()).format(date)
+        return api.getMeal(formattedDate).await()
     }
 }
