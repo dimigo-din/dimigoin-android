7dfaaefc09785a659c410d6928529fbfd47c379e
SeungHyunMin <contact@seunghyun.in>
2020-11-08 16:50:22 +0900

    refactor: buildRetrofit()

---
+++ app/src/main/java/in/dimigo/dimigoin/data/api/AuthorizationInterceptor.kt
@@ -0,0 +1,19 @@
+package `in`.dimigo.dimigoin.data.api
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class AuthorizationInterceptor(private val jwtToken: String?) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val isAuthorizationHeaderExists = chain.request().header("Authorization") != null
+
+        val request = if (jwtToken == null || isAuthorizationHeaderExists) {
+            chain.request()
+        } else {
+            chain.request().newBuilder()
+                .addHeader("Authorization", jwtToken)
+                .build()
+        }
+        return chain.proceed(request)
+    }
+}
--- app/src/main/java/in/dimigo/dimigoin/di/NetworkModule.kt
+++ app/src/main/java/in/dimigo/dimigoin/di/NetworkModule.kt
@@ -1,5 +1,6 @@
 package `in`.dimigo.dimigoin.di
 
+import `in`.dimigo.dimigoin.data.api.AuthorizationInterceptor
 import `in`.dimigo.dimigoin.data.api.DimigoinApi
 import `in`.dimigo.dimigoin.ui.login.LoginActivity.Companion.KEY_TOKEN
 import android.content.SharedPreferences
@@ -10,29 +11,25 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 val networkModule = module {
+    single {
+        val accessToken = getAccessToken(get())
+        AuthorizationInterceptor(accessToken)
+    }
+    single { buildOkHttpClient(get<AuthorizationInterceptor>()) }
     single { buildRetrofit(get()) }
 
     single { get<Retrofit>().create(DimigoinApi::class.java) }
 }
 
-private fun buildRetrofit(sharedPreferences: SharedPreferences): Retrofit {
-    val token = sharedPreferences.getString(KEY_TOKEN, null)
+private fun getAccessToken(sharedPreferences: SharedPreferences) = sharedPreferences.getString(KEY_TOKEN, null)
 
-    val httpClient = OkHttpClient.Builder()
-        .addInterceptor(Interceptor { chain ->
-            val isAuthorizationHeaderExists = chain.request().header("Authorization") != null
-
-            val request = if (token == null || isAuthorizationHeaderExists) {
-                chain.request()
-            } else {
-                chain.request().newBuilder()
-                    .addHeader("Authorization", token)
-                    .build()
-            }
-            return@Interceptor chain.proceed(request)
-        })
+private fun buildOkHttpClient(interceptor: Interceptor): OkHttpClient {
+    return OkHttpClient.Builder()
+        .addInterceptor(interceptor)
         .build()
+}
 
+private fun buildRetrofit(httpClient: OkHttpClient): Retrofit {
     return Retrofit.Builder()
         .baseUrl(DimigoinApi.BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
