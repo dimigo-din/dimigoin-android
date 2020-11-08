a95b96a0d0ddcd553f9957b23655333bdb5ba431
SeungHyunMin <contact@seunghyun.in>
2020-11-08 15:56:41 +0900

    feat: add interceptor to add Authorization header

--- app/src/main/java/in/dimigo/dimigoin/di/NetworkModule.kt
+++ app/src/main/java/in/dimigo/dimigoin/di/NetworkModule.kt
@@ -1,17 +1,41 @@
 package `in`.dimigo.dimigoin.di
 
 import `in`.dimigo.dimigoin.data.api.DimigoinApi
+import `in`.dimigo.dimigoin.ui.login.LoginActivity.Companion.KEY_TOKEN
+import android.content.SharedPreferences
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 val networkModule = module {
-    single {
-        Retrofit.Builder()
-            .baseUrl(DimigoinApi.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-    }
+    single { buildRetrofit(get()) }
 
     single { get<Retrofit>().create(DimigoinApi::class.java) }
+}
+
+private fun buildRetrofit(sharedPreferences: SharedPreferences): Retrofit {
+    val token = sharedPreferences.getString(KEY_TOKEN, null)
+
+    val httpClient = OkHttpClient.Builder()
+        .addInterceptor(Interceptor { chain ->
+            val isAuthorizationHeaderExists = chain.request().header("Authorization") != null
+
+            val request = if (token == null || isAuthorizationHeaderExists) {
+                chain.request()
+            } else {
+                chain.request().newBuilder()
+                    .addHeader("Authorization", token)
+                    .build()
+            }
+            return@Interceptor chain.proceed(request)
+        })
+        .build()
+
+    return Retrofit.Builder()
+        .baseUrl(DimigoinApi.BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(httpClient)
+        .build()
 }
