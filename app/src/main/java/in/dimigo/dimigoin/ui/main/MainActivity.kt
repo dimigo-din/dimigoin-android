33d8f2a292608d9245d0d6e80d0194a84c0b9d27
SeungHyunMin <contact@seunghyun.in>
2020-11-12 22:06:50 +0900

    feat: bottom navigation view on MainActivity

--- app/src/main/java/in/dimigo/dimigoin/ui/main/MainActivity.kt
+++ app/src/main/java/in/dimigo/dimigoin/ui/main/MainActivity.kt
@@ -1,12 +1,20 @@
 package `in`.dimigo.dimigoin.ui.main
 
 import `in`.dimigo.dimigoin.R
+import `in`.dimigo.dimigoin.databinding.ActivityMainBinding
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        initView(binding)
+    }
+
+    private fun initView(binding: ActivityMainBinding) {
+        binding.mainBottomNav.selectedItemId = R.id.main
     }
 }
