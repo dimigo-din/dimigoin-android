a09e7fe1d1c2b14a568507671d967527a6d481b3
wkdwhddn06 <wkdwhddn06@naver.com>
2020-11-13 21:34:56 +0900

    feat: add meal viewmodel

--- app/src/main/java/in/dimigo/dimigoin/ui/main/fragment/meal/MealFragment.kt
+++ app/src/main/java/in/dimigo/dimigoin/ui/main/fragment/meal/MealFragment.kt
@@ -6,11 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import org.koin.android.viewmodel.ext.android.viewModel
 
 class MealFragment : Fragment() {
+    private val viewModel: MealViewModel by viewModel()
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val binding = FragmentMealBinding.inflate(inflater, container, false)
         binding.lifecycleOwner = viewLifecycleOwner
+        binding.vm = viewModel
 
         return binding.root
     }
---
+++ app/src/main/java/in/dimigo/dimigoin/ui/main/fragment/meal/MealViewModel.kt
@@ -0,0 +1,5 @@
+package `in`.dimigo.dimigoin.ui.main.fragment.meal
+
+import androidx.lifecycle.ViewModel
+
+class MealViewModel : ViewModel()
