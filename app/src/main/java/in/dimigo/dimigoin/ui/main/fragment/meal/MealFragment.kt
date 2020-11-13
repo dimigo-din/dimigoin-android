6cff96cb6ef96f77d471a2b520082e5f91099479
wkdwhddn06 <wkdwhddn06@naver.com>
2020-11-13 21:27:45 +0900

    feat: finish fragment meal layout

--- app/src/main/java/in/dimigo/dimigoin/ui/main/fragment/meal/MealFragment.kt
+++ app/src/main/java/in/dimigo/dimigoin/ui/main/fragment/meal/MealFragment.kt
@@ -1,6 +1,6 @@
 package `in`.dimigo.dimigoin.ui.main.fragment.meal
 
-import `in`.dimigo.dimigoin.R
+import `in`.dimigo.dimigoin.databinding.FragmentMealBinding
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +9,9 @@ import androidx.fragment.app.Fragment
 
 class MealFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_meal, container, false)
+        val binding = FragmentMealBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        return binding.root
     }
 }
