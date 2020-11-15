954cc12e7ec5f9b251f03011a4013ae7a2a0a181
SeungHyunMin <contact@seunghyun.in>
2020-11-15 23:30:57 +0900

    feat(layout): ingang fragment layout

--- app/src/main/java/in/dimigo/dimigoin/ui/main/fragment/ingang/IngangFragment.kt
+++ app/src/main/java/in/dimigo/dimigoin/ui/main/fragment/ingang/IngangFragment.kt
@@ -1,6 +1,6 @@
 package `in`.dimigo.dimigoin.ui.main.fragment.ingang
 
-import `in`.dimigo.dimigoin.R
+import `in`.dimigo.dimigoin.databinding.FragmentIngangBinding
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment
 
 class IngangFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_ingang, container, false)
+        val binding = FragmentIngangBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
