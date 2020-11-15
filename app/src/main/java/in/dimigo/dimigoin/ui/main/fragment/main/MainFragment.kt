8c9e2cb77d53a0ba3d059ae2db7d763d82c780b1
SeungHyunMin <contact@seunghyun.in>
2020-11-15 15:45:28 +0900

    refactor: improve code readability

--- app/src/main/java/in/dimigo/dimigoin/ui/main/fragment/main/MainFragment.kt
+++ app/src/main/java/in/dimigo/dimigoin/ui/main/fragment/main/MainFragment.kt
@@ -11,6 +11,7 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
+import com.rd.PageIndicatorView
 import java.util.*
 
 class MainFragment : Fragment() {
@@ -29,28 +30,41 @@ class MainFragment : Fragment() {
 
     private fun initView(binding: FragmentMainBinding) = with(binding) {
         mealViewPager.apply {
-            offscreenPageLimit = 3
-            val offsetPx = resources.getDimensionPixelOffset(R.dimen.meal_view_pager_page_offset)
-            val pageGapPx = resources.getDimensionPixelOffset(R.dimen.meal_view_pager_page_gap)
-            setPageTransformer { page, position ->
-                val offset = position * -(2 * offsetPx + pageGapPx)
-                page.translationX = offset
-            }
+            disableOverScrollMode()
+            setDefaultSelectedItem(mealPageIndicator)
+            applyPageIndicator(mealPageIndicator)
+            applyCarouselEffect()
             adapter = MealCardAdapter(this@MainFragment)
-
-            val child = getChildAt(0)
-            if (child is RecyclerView) child.overScrollMode = View.OVER_SCROLL_NEVER
-
-            val currentCardPosition = getCurrentMealTime().ordinal
-            setCurrentItem(currentCardPosition, false)
-            mealPageIndicator.setSelected(currentCardPosition)
-
-            registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
-                override fun onPageSelected(position: Int) {
-                    mealPageIndicator.selection = position
-                }
-            })
         }
+    }
+
+    private fun ViewPager2.applyCarouselEffect() {
+        offscreenPageLimit = 3
+        val offsetPx = resources.getDimensionPixelOffset(R.dimen.meal_view_pager_page_offset)
+        val pageGapPx = resources.getDimensionPixelOffset(R.dimen.meal_view_pager_page_gap)
+        setPageTransformer { page, position ->
+            val offset = position * -(2 * offsetPx + pageGapPx)
+            page.translationX = offset
+        }
+    }
+
+    private fun ViewPager2.disableOverScrollMode() {
+        val child = getChildAt(0)
+        if (child is RecyclerView) child.overScrollMode = View.OVER_SCROLL_NEVER
+    }
+
+    private fun ViewPager2.setDefaultSelectedItem(mealPageIndicator: PageIndicatorView) {
+        val currentCardPosition = getCurrentMealTime().ordinal
+        setCurrentItem(currentCardPosition, false)
+        mealPageIndicator.setSelected(currentCardPosition)
+    }
+
+    private fun ViewPager2.applyPageIndicator(mealPageIndicator: PageIndicatorView) {
+        registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                mealPageIndicator.selection = position
+            }
+        })
     }
 
     private fun getCurrentMealTime() = when (Calendar.getInstance().get(Calendar.HOUR_OF_DAY)) {
