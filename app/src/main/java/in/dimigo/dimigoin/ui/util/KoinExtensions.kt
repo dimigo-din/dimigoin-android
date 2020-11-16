918add853c62fd6327305909fc49080ab5fd5984
SeungHyunMin <contact@seunghyun.in>
2020-11-16 09:11:39 +0900

    feat: edit sharedGraphViewModel()

--- app/src/main/java/in/dimigo/dimigoin/ui/util/KoinExtensions.kt
+++ app/src/main/java/in/dimigo/dimigoin/ui/util/KoinExtensions.kt
@@ -4,10 +4,17 @@ import androidx.annotation.IdRes
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
 import androidx.navigation.fragment.findNavController
-import org.koin.android.viewmodel.ViewModelOwner
-import org.koin.android.viewmodel.ext.android.sharedViewModel
+import org.koin.android.ext.android.getKoin
+import org.koin.android.viewmodel.ViewModelParameter
+import org.koin.android.viewmodel.koin.getViewModel
+import org.koin.core.parameter.ParametersDefinition
+import org.koin.core.qualifier.Qualifier
 
-inline fun <reified VM : ViewModel> Fragment.sharedGraphViewModel(@IdRes navGraphId: Int) =
-    sharedViewModel<VM>(owner = {
-        ViewModelOwner.from(findNavController().getViewModelStoreOwner(navGraphId))
-    })
+inline fun <reified VM : ViewModel> Fragment.sharedGraphViewModel(
+    @IdRes navGraphId: Int,
+    qualifier: Qualifier? = null,
+    noinline parameters: ParametersDefinition? = null
+) = lazy {
+    val store = findNavController().getViewModelStoreOwner(navGraphId).viewModelStore
+    getKoin().getViewModel(ViewModelParameter(VM::class, qualifier, parameters, store))
+}
