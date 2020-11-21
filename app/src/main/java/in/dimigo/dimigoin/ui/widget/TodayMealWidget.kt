b5834fbdf7133c23ec9eb674d8a5e867be6eb784
SeungHyunMin <contact@seunghyun.in>
2020-11-22 02:27:55 +0900

    feat: update today's meal widget everyday #14

--- app/src/main/java/in/dimigo/dimigoin/ui/widget/TodayMealWidget.kt
+++ app/src/main/java/in/dimigo/dimigoin/ui/widget/TodayMealWidget.kt
@@ -3,15 +3,29 @@ package `in`.dimigo.dimigoin.ui.widget
 import `in`.dimigo.dimigoin.R
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.widget.RemoteViews
 
 class TodayMealWidget : AppWidgetProvider() {
+    private var updateAlarm: WidgetUpdateAlarm? = null
+
+    override fun onReceive(context: Context, intent: Intent) {
+        super.onReceive(context, intent)
+
+        if (intent.action == ACTION_UPDATE_WIDGET) {
+            val appWidgetManager = AppWidgetManager.getInstance(context)
+            val mealWidget = ComponentName(context, this::class.java)
+            onUpdate(context, appWidgetManager, appWidgetManager.getAppWidgetIds(mealWidget))
+        }
+    }
+
     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
         for (appWidgetId in appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId)
         }
+        appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.mealListView)
     }
 
     private fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
@@ -21,5 +35,19 @@ class TodayMealWidget : AppWidgetProvider() {
         }
         remoteViews.setRemoteAdapter(R.id.mealListView, intent)
         appWidgetManager.updateAppWidget(appWidgetId, remoteViews)
+    }
+
+    override fun onEnabled(context: Context) {
+        val intent = Intent(context, this::class.java)
+        updateAlarm = WidgetUpdateAlarm(context, intent)
+        updateAlarm?.startAlarm(listOf(0))
+    }
+
+    override fun onDisabled(context: Context) {
+        updateAlarm?.stopAlarm()
+    }
+
+    companion object {
+        const val ACTION_UPDATE_WIDGET = "UPDATE_WIDGET"
     }
 }
---
+++ app/src/main/java/in/dimigo/dimigoin/ui/widget/WidgetUpdateAlarm.kt
@@ -0,0 +1,55 @@
+package `in`.dimigo.dimigoin.ui.widget
+
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import java.util.*
+import kotlin.collections.ArrayList
+
+class WidgetUpdateAlarm(private val context: Context, private val intent: Intent) {
+    private val alarmManager: AlarmManager by lazy {
+        context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+    }
+    private val pendingIntents = ArrayList<PendingIntent>()
+
+    init {
+        intent.action = TodayMealWidget.ACTION_UPDATE_WIDGET
+    }
+
+    fun startAlarm(updateHours: List<Int>) {
+        val calendar = Calendar.getInstance().apply {
+            set(Calendar.MINUTE, 0)
+            set(Calendar.SECOND, 0)
+            set(Calendar.MILLISECOND, 0)
+        }
+
+        for (updateHour in updateHours) {
+            calendar.apply {
+                set(Calendar.HOUR_OF_DAY, updateHour)
+            }
+            val pendingIntent = getPendingIntent()
+            pendingIntents.add(pendingIntent)
+
+            alarmManager.setRepeating(
+                AlarmManager.RTC_WAKEUP,
+                calendar.timeInMillis,
+                AlarmManager.INTERVAL_DAY,
+                pendingIntent
+            )
+        }
+    }
+
+    fun stopAlarm() {
+        for (pendingIntent in pendingIntents)
+            alarmManager.cancel(pendingIntent)
+        pendingIntents.clear()
+    }
+
+    private fun getPendingIntent() = PendingIntent.getBroadcast(
+        context,
+        System.currentTimeMillis().toInt(),
+        intent,
+        PendingIntent.FLAG_CANCEL_CURRENT
+    )
+}
