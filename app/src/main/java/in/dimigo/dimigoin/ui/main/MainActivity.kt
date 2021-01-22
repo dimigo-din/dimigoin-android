20665a2f3c8b0d1a2f387aaa034d57c486412588
SeungHyun Min <contact@seunghyun.in>
2021-01-23 00:21:58 +0900

    feat: disable bottom navigation bar when card is showing

--- app/src/main/java/in/dimigo/dimigoin/ui/main/MainActivity.kt
+++ app/src/main/java/in/dimigo/dimigoin/ui/main/MainActivity.kt
@@ -5,6 +5,7 @@ import `in`.dimigo.dimigoin.databinding.ActivityMainBinding
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.forEach
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -20,20 +21,28 @@ class MainActivity : AppCompatActivity() {
 
     @Suppress("DEPRECATION")
     fun enterFullScreen() {
+        enableBottomNavBar(false)
         binding.motionLayout.transitionToEnd()
         window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 or View.SYSTEM_UI_FLAG_FULLSCREEN
                 or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
     }
 
+    fun showBottomNavBar() {
+        binding.motionLayout.transitionToStart()
+    }
+
     @Suppress("DEPRECATION")
     fun exitFullScreen() {
+        enableBottomNavBar(true)
         binding.mainBottomNav.visibility = View.VISIBLE
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
     }
 
-    fun showBottomNavBar() {
-        binding.motionLayout.transitionToStart()
+    private fun enableBottomNavBar(enable: Boolean) = with(binding.mainBottomNav) {
+        menu.forEach {
+            it.isEnabled = enable
+        }
     }
 
     private fun initView() {
