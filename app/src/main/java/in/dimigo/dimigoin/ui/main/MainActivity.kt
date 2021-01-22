b8ba3180633cffcdd56c4932d686e8e8a5666c18
SeungHyun Min <contact@seunghyun.in>
2021-01-22 23:18:03 +0900

    feat: show black background behind the card

--- app/src/main/java/in/dimigo/dimigoin/ui/main/MainActivity.kt
+++ app/src/main/java/in/dimigo/dimigoin/ui/main/MainActivity.kt
@@ -3,20 +3,40 @@ package `in`.dimigo.dimigoin.ui.main
 import `in`.dimigo.dimigoin.R
 import `in`.dimigo.dimigoin.databinding.ActivityMainBinding
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        initView(binding)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        initView()
     }
 
-    private fun initView(binding: ActivityMainBinding) {
+    @Suppress("DEPRECATION")
+    fun enterFullScreen() {
+        binding.motionLayout.transitionToEnd()
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+    }
+
+    @Suppress("DEPRECATION")
+    fun exitFullScreen() {
+        binding.mainBottomNav.visibility = View.VISIBLE
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+    }
+
+    fun showBottomNavBar() {
+        binding.motionLayout.transitionToStart()
+    }
+
+    private fun initView() {
         val fragment = supportFragmentManager.findFragmentById(R.id.mainFragmentContainer) as NavHostFragment
         val navController = fragment.navController
         binding.mainBottomNav.apply {
--- app/src/main/java/in/dimigo/dimigoin/ui/main/fragment/card/CardFragment.kt
+++ app/src/main/java/in/dimigo/dimigoin/ui/main/fragment/card/CardFragment.kt
@@ -4,16 +4,22 @@ import `in`.dimigo.dimigoin.R
 import `in`.dimigo.dimigoin.data.service.DimigoinService
 import `in`.dimigo.dimigoin.data.util.UserDataStore
 import `in`.dimigo.dimigoin.databinding.FragmentCardBinding
+import `in`.dimigo.dimigoin.ui.main.MainActivity
+import android.graphics.Rect
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.core.view.updatePadding
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 
 class CardFragment : Fragment() {
     private lateinit var binding: FragmentCardBinding
     private var isCardShowing = false
+    private val mainActivity: MainActivity?
+        get() = activity as? MainActivity
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentCardBinding.inflate(inflater, container, false)
@@ -36,20 +42,65 @@ class CardFragment : Fragment() {
         cardBackLayout.cameraDistance = cameraDistance
 
         cardFrontLayout.setOnClickListener {
-            isCardShowing = if (isCardShowing) {
-                cardMotionLayout.transitionToStart()
-                false
-            } else {
-                cardMotionLayout.transitionToEnd()
-                true
-            }
+            if (isCardShowing) hideCard()
+            else showCard()
         }
+
+        cardMotionLayout.setTransitionListener(transitionListener)
 
         if (UserDataStore.userData.photo.isNotEmpty()) {
             Glide.with(requireContext())
                 .load(DimigoinService.getProfileUrl(UserDataStore.userData.photo.last()))
                 .into(profileImage)
         }
+    }
+
+    private fun showCard() {
+        mainActivity?.enterFullScreen()
+        binding.cardMotionLayout.updatePadding(top = getStatusBarHeight(), bottom = getNavigationBarHeight())
+        binding.cardMotionLayout.transitionToEnd()
+        isCardShowing = true
+    }
+
+    private fun hideCard() {
+        mainActivity?.showBottomNavBar()
+        binding.cardMotionLayout.transitionToStart()
+        isCardShowing = false
+    }
+
+    private val transitionListener = object : MotionLayout.TransitionListener {
+        override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+            if (currentId == R.id.card_start) onCardHidden()
+        }
+
+        override fun onTransitionStarted(motionLayout: MotionLayout?, startId: Int, endId: Int) {}
+        override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {}
+        override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {}
+    }
+
+    private fun onCardHidden() {
+        mainActivity?.exitFullScreen()
+        binding.cardMotionLayout.updatePadding(top = 0, bottom = 0)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding.cardMotionLayout.removeTransitionListener(transitionListener)
+    }
+
+    private fun getStatusBarHeight(): Int {
+        val rectangle = Rect()
+        val window = activity?.window ?: return 0
+        window.decorView.getWindowVisibleDisplayFrame(rectangle)
+        return rectangle.top
+    }
+
+    private fun getNavigationBarHeight(): Int {
+        val resources = context?.resources ?: return 0
+        val resourceId = resources.getIdentifier("navigation_bar_height", "dimen", "android")
+        return if (resourceId > 0) {
+            resources.getDimensionPixelSize(resourceId)
+        } else 0
     }
 
     companion object {
