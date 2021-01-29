3e61465cd103b911f39e08fe9673442779476289
wkdwhddn06 <wkdwhddn06@naver.com>
2021-01-29 18:47:22 +0900

    refactor: rename meal fragment into weekly meal fragment

--- app/src/main/java/in/dimigo/dimigoin/ui/main/fragment/meal/MealFragment.kt
+++ app/src/main/java/in/dimigo/dimigoin/ui/main/fragment/meal/MealFragment.kt
@@ -1,27 +1,5 @@
 package `in`.dimigo.dimigoin.ui.main.fragment.meal
 
-import `in`.dimigo.dimigoin.R
-import `in`.dimigo.dimigoin.databinding.FragmentMealBinding
-import `in`.dimigo.dimigoin.ui.main.fragment.util.getFormattedToday
-import `in`.dimigo.dimigoin.ui.util.sharedGraphViewModel
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import me.everything.android.ui.overscroll.OverScrollDecoratorHelper
 
-class MealFragment : Fragment() {
-    private val viewModel: MealViewModel by sharedGraphViewModel(R.id.main_nav_graph)
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val binding = FragmentMealBinding.inflate(inflater, container, false)
-        binding.apply {
-            lifecycleOwner = viewLifecycleOwner
-            vm = viewModel
-            date = getFormattedToday(getString(R.string.date_format))
-            OverScrollDecoratorHelper.setUpOverScroll(mealScrollView)
-        }
-        return binding.root
-    }
-}
+class MealFragment : Fragment()
