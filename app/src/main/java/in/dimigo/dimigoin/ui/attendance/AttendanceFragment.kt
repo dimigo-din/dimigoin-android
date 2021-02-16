dc1ecb82af686c92991ace95c8968b4a1373174a
wkdwhddn06 <wkdwhddn06@naver.com>
2021-02-16 16:39:16 +0900

    feat: bind layout with attendance fragment

--- app/src/main/java/in/dimigo/dimigoin/ui/attendance/AttendanceFragment.kt
+++ app/src/main/java/in/dimigo/dimigoin/ui/attendance/AttendanceFragment.kt
@@ -1,5 +1,19 @@
 package `in`.dimigo.dimigoin.ui.attendance
 
+import `in`.dimigo.dimigoin.databinding.FragmentAttendanceBinding
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class AttendanceFragment : Fragment()
+class AttendanceFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        val binding = FragmentAttendanceBinding.inflate(inflater, container, false).apply {
+            lifecycleOwner = viewLifecycleOwner
+        }
+
+        return binding.root
+    }
+}
