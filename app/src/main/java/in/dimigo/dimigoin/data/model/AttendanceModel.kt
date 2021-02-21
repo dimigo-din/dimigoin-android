1c2ade2bcbe7a7188e19eaa41285ddd41a979f06
wkdwhddn06 <wkdwhddn06@naver.com>
2021-02-21 21:03:09 +0900

    feat: design history dialog

--- app/src/main/java/in/dimigo/dimigoin/data/model/AttendanceModel.kt
+++ app/src/main/java/in/dimigo/dimigoin/data/model/AttendanceModel.kt
@@ -9,10 +9,11 @@ data class AttendanceLogsResponseModel(
 
 data class AttendanceLogModel(
     val student: UserModel,
-    val date: String,
+    val date: Date,
     val place: PlaceModel,
     val remark: String,
-    @SerializedName("updatedAt") val time: Date
+    val updatedBy: UserModel? = null,
+    @SerializedName("updatedAt") val time: Date,
 )
 
 data class AttendanceLogRequestModel(
