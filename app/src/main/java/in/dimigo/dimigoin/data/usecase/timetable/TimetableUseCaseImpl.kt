da8a568a8600d238edd2f210aee0de64def05c00
nutyworks <nutyworks@gmail.com>
2021-05-10 12:54:39 +0900

    fix: load timetables with empty timetable for a day

--- app/src/main/java/in/dimigo/dimigoin/data/usecase/timetable/TimetableUseCaseImpl.kt
+++ app/src/main/java/in/dimigo/dimigoin/data/usecase/timetable/TimetableUseCaseImpl.kt
@@ -1,12 +1,14 @@
 package `in`.dimigo.dimigoin.data.usecase.timetable
 
 import `in`.dimigo.dimigoin.data.service.DimigoinService
+import `in`.dimigo.dimigoin.data.util.DateUtil
 import `in`.dimigo.dimigoin.data.util.Result
 import `in`.dimigo.dimigoin.data.util.UserDataStore
 import `in`.dimigo.dimigoin.data.util.safeApiCall
 import `in`.dimigo.dimigoin.ui.item.SubjectItem
 import retrofit2.await
 import java.time.DayOfWeek
+import java.time.LocalDate
 
 class TimetableUseCaseImpl(
     private val service: DimigoinService,
@@ -19,17 +21,20 @@ class TimetableUseCaseImpl(
         val timetables = service.getWeeklyTimetable(grade, klass)
             .await()
             .dailyTimetables
-        val subjects = mutableListOf<SubjectItem?>()
+        val now = LocalDate.now()
 
-        repeat(7) { i ->
-            repeat(5) { j ->
-                if (timetables[j].sequence.size > i)
-                    subjects.add(SubjectItem(timetables[j].sequence[i], DayOfWeek.of(j + 1)))
-                else
-                    subjects.add(null)
-            }
+        return@safeApiCall List(5) {
+            now.with(DayOfWeek.MONDAY).plusDays(it.toLong()).format(DateUtil.dateFormatter)
+        }.map {
+            timetables.find { timetable -> timetable.date == it }?.sequence ?: arrayOf()
+        }.map {
+            it.plus(arrayOfNulls(7)).take(7)
+        }.let {
+            List(7) { time ->
+                List(5) { day ->
+                    SubjectItem(it[day][time], DayOfWeek.of(day + 1))
+                }
+            }.flatMap { it }
         }
-
-        return@safeApiCall subjects
     }
 }
