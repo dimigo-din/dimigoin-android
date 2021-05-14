1e04173f625da7677fc602d5567eecb1e0bdf5be
Beomjun Kim <nutyworks@gmail.com>
2021-05-15 01:27:11 +0900

    fix: remove flickering when re-selecting the same fragment (#68)
    * fix: remove flickering when re-selecting a same fragment
    
    * chore: correct grammar
    
    Changed "a same" to "the same".
    This commit fixes grammar error in commit ae0dfa5.

--- app/src/main/java/in/dimigo/dimigoin/ui/main/MainActivity.kt
+++ app/src/main/java/in/dimigo/dimigoin/ui/main/MainActivity.kt
@@ -71,6 +71,9 @@ class MainActivity : BaseActivity() {
             setupWithNavController(navController)
             selectedItemId = viewModel.navigationPosition
             bottomNavBarBottomPadding = paddingBottom
+            setOnNavigationItemReselectedListener {
+                // Do nothing: it was intended to prevent flickering when re-selecting the same fragment.
+            }
         }
     }
 }
